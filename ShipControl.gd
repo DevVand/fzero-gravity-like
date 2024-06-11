111f3c0065f706f87199429b68923e0c7cc2e0d5
Haru Sales <linearharu@gmail.com>
2024-06-11 00:45:53 -0300

    funciona

--- ShipControl.gd
+++ ShipControl.gd
@@ -1,16 +1,17 @@
-extends StaticBody3D
+extends CharacterBody3D
 
 @export var ray: RayCast3D 
 
-@export var currentLine: Path3D
+@onready var currentLine = $"../Path3D" as Path3D
 @export var currentPath: PathFollow3D 
+
+
 
 
 var dir: Vector3
 
 @export var acceleration: float
 var gVelocity: Vector3
-var velocity: Vector3
 var linearVelocity: float
 
 var lVelocity: float = 10
@@ -20,6 +21,7 @@ var lVelocity: float = 10
 var rSpeed: float
 @export var rFriction: float = .8
 
+@export var gravity:float = 20
 @export var mass: float = 1
 @export var maxSpeed: float
 @export var groundMagnet: float
@@ -29,35 +31,32 @@ var rSpeed: float
 @export var groundMagnetHeighSpeed: float = 10
 # Called when the node enters the scene tree for the first time.
 var curve:Curve3D
+var state: Callable
+var delta: float 
+
 
 func _ready():
 	#currentPath = get_tree().current_scene.get_node("Path3D/PathFollow3D")
 	#currentLine = get_tree().current_scene.get_node("$../track")
 	
+
 	curve = currentLine.curve
-	pass # Replace with function body.
+	state = onTrack
 
-
-func _physics_process(delta):
-	
-	
-	pass
-
-		#transform.origin.lerp( (p + transform.basis.y * groundMagnetDist), lerpV ) #* groundMagnetLerp * delta)
-
-
-#		rgbd.apply_central_force( transform.basis.y * groundMagnet * delta)
-#	rgbd.apply_impulse(transform.basis.z * delta / speed * rgbd.mass)
- 
 func _process(delta):
 	
+	if get_slide_collision_count() > 0:
+		state=onCrash
+	self.delta=delta
+	state.call()
+
+func onTrack():
 	#dir=Vector3.ZERO
-	var i:Vector2 = Input.get_vector("left","right","reverse","acel")
-	lVelocity += acceleration * i.y
-#	velocity+=transform.basis.z * acceleration * i.y
-	rSpeed+=(-rAcceleration*i.x)*delta
-	
-	if ray.is_colliding():
+	if ray.is_colliding(): 
+		var i:Vector2 = Input.get_vector("left","right","reverse","acel")
+		lVelocity += acceleration * i.y
+	#	velocity+=transform.basis.z * acceleration * i.y
+		rSpeed+=(-rAcceleration*i.x)*delta
 		
 		var p: Vector3 = ray.get_collision_point()
 		var desiredPos: Vector3 = p + transform.basis.y * groundMagnetDist
@@ -80,17 +79,58 @@ func _process(delta):
 		t.basis.x = -t.basis.z.cross(n)
 		t.basis = t.basis.orthonormalized()
 		
-		transform.basis = basis.slerp(t.basis,lerpR) #transform.interpolate_with(t,lerpV)
+		transform.basis = basis.slerp(t.basis,lerpR).orthonormalized() #transform.interpolate_with(t,lerpV)
 		
 		position = position.lerp(desiredPos, lerpV)
-	velocity = basis.z * lVelocity
-	move_and_collide((velocity)*delta)
-	rotate(transform.basis.y.normalized(),rSpeed)
-	rSpeed*=rFriction
-#	velocity*=friction+(1-friction if i.length()<.1 else 0)
+		velocity = basis.z * lVelocity
+		move_and_collide((velocity)*delta)
+		rotate(transform.basis.y.normalized(),rSpeed)
+		rSpeed*=rFriction
+	#	velocity*=friction+(1-friction if i.length()<.1 else 0)
+		
+		lVelocity*=friction+(1-friction if i.length()<.1 else 0)
+		#speed*=friction
+	else:
+		_on_GetOffTrack()
+
+func offTrack():
 	
-	lVelocity*=friction+(1-friction if i.length()<.1 else 0)
-	#speed*=friction
+	var i:Vector2 = Input.get_vector("left","right","reverse","acel")
+	lVelocity += acceleration * i.y if i.y<0 else 0 
+#	velocity+=transform.basis.z * acceleration * i.y
+	rSpeed+=(rAcceleration*i.x)*delta*.85
+	
+	velocity+=Vector3.DOWN * gravity
+	move_and_collide((velocity)*delta)
+	rotate(transform.basis.z.normalized(),rSpeed)
+	rSpeed*=rFriction
+	
+	pass
+signal crashed
+
+func onCrash():
+	lVelocity=0
+	velocity=Vector3.ZERO
+	crashed.emit()
+	state=onCrash
+
+
+signal getOnTrack
+func _on_GetOnTrack():
+	getOnTrack.emit()
+	state = onTrack
+	
+signal getOffTrack
+func _on_GetOffTrack():
+	getOffTrack.emit()
+	state = offTrack
+	
+		#transform.origin.lerp( (p + transform.basis.y * groundMagnetDist), lerpV ) #* groundMagnetLerp * delta)
+
+
+#		rgbd.apply_central_force( transform.basis.y * groundMagnet * delta)
+#	rgbd.apply_impulse(transform.basis.z * delta / speed * rgbd.mass)
+ 
 
 #func _unhandled_input(event):
 #	if event is InputEventKey:
@@ -102,5 +142,3 @@ func _process(delta):
 #			rSpeed+=(rotationAcceleration)
 #		if event.is_action_pressed("ui_left"):
 #			rSpeed-=(rotationAcceleration)
-		
-
