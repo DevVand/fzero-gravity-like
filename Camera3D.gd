794c074b6ee7751bd505e0fc10ec5858174a46da
DevVand <61861793+DevVand@users.noreply.github.com>
2024-07-24 16:05:00 -0300

    funciona

--- Camera3D.gd
+++ Camera3D.gd
@@ -14,6 +14,7 @@ var initialFov: float = 65
 @export var fovChange: float = .4
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	initialFov = fov
 	top_level=true
 	offset = Vector3.ZERO # position - target.position 
 	pass # Replace with function body.
@@ -21,9 +22,9 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	var d:float= position.distance_to(target.position + offset)
-	position = position.lerp(target.global_position + offset,lerpSpeed*delta)
-	look_at(targetView.position, targetView.basis.y)
+	var d:float= abs(global_position.distance_to(target.global_position + offset))
+	global_position= global_position.lerp(target.global_position + offset,lerpSpeed*delta)
+	look_at(targetView.global_position, targetView.basis.y)
 	#change fov based of offset distance
 	fov = min(130, initialFov + d*fovChange)
 
--- basicMove.gd
+++ basicMove.gd
@@ -1,6 +1,8 @@
 extends CharacterBody3D
 
 @export var acc: float = 150
+
+@export var grav: float = 20
 var vel: Vector3
 @export var friction: float = .95
 
@@ -12,26 +14,36 @@ var tVel: float
 var rVel: float
 @export var rFriction: float = .7
 
-
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-@export var grav: float = 10
-
 @export var targetGroundPos: Node3D
 @export var groundRaycast: RayCast3D
 @export var animation_player: AnimationPlayer
-
+var extForces = Vector3.ZERO
 var state="air"
+@onready var gpu_particles_3d = $GPUParticles3D
 
 func _physics_process(delta):
 	var i:Vector2 = Input.get_vector("right","left","reverse","acel")
-	var dir = (transform.basis * Vector3(i.x, 0, i.y)).normalized()
+	var dir = (Vector3(i.x, 0, i.y)).normalized()
+	var part = gpu_particles_3d.process_material as ParticleProcessMaterial
+	
+	if dir or state == "air":
+		#part.direction = -vel.normalized()
+		#part.initial_velocity_min = vel.length() *.1
+		gpu_particles_3d.lifetime=1
+	else:
+		gpu_particles_3d.lifetime=4
+		#part.initial_velocity_max = 0
+		
+	gpu_particles_3d.process_material=part
 	
 	match state:
 		"air":
-			vel -= Vector3.UP * grav * delta
+			extForces -= Vector3.UP * grav * delta
 			rVel -= i.x * rAcc * delta
 			rotate(basis.z, rVel)
 			rVel *= rFriction
+			
+			#rotate(basis.x, i.y * rAcc * delta)
 			if groundRaycast.is_colliding():
 				goToGround()
 		"ground":
@@ -46,13 +58,15 @@ func _physics_process(delta):
 				vel.z *= friction
 				var n = groundRaycast.get_collision_normal()
 				var xform: Transform3D = align_with_y(global_transform, n)
-				global_transform = global_transform.interpolate_with(xform, .15)
+				global_transform = global_transform.interpolate_with(xform, .05)
 				#transform.basis = basis.slerp(xform.basis,.5).orthonormalized()
 				#xform.basis = xform.basis.orthonormalized()
 				position = position.lerp(targetGroundPos.global_position,.25)
 			else:
 				goToAir()
 	velocity += Utils.RotateToBasis(vel,basis)
+	
+	velocity += extForces
 	move_and_slide()
 	velocity = Vector3.ZERO
 func goToAir():
@@ -60,10 +74,11 @@ func goToAir():
 	animation_player.play(state)
 
 func goToGround():
+	extForces = Vector3.ZERO
 	vel.y=0
 	var n = groundRaycast.get_collision_normal()
 	var xform: Transform3D = align_with_y(global_transform, n)
-	global_transform = global_transform.interpolate_with(xform, .8)
+	global_transform = global_transform.interpolate_with(xform, .65)
 	state="ground"
 	animation_player.play(state)
 
